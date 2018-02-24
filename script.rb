3e3c7337e9fb7ecb30c705430b2aff4a7754f88f
Nikkie Grom <nikkie@nikkie.ru>
2018-02-24 16:01:47 +0300

    done

--- script.rb
+++ script.rb
@@ -1,20 +1,20 @@
 require 'pdf-reader'
 
-reader = PDF::Reader.new("pharm.pdf")
+reader = PDF::Reader.new('pharm.pdf')
 
-question_blocks_array = Array.new
-questions_array = Array.new
-answers_array = Array.new
+question_blocks_array = []
+questions_array = []
+answers_array = []
 
 regexp_question_block = /(?<=[\]][ ])(.+?)(?=\d{1,5})/mo
 regexp_question = /((.+?)(?=[А][)]))/mo
-regexp_answer = /(?=\))(.+?)(?=(\b[)]|\z))/mo
+regexp_answer = /(?<=\))(.+?)(?=([А-Г][)]|\z))/mo
 
 reader.pages.each do |page|
   page.text.scan(regexp_question_block) do |m|
     question_blocks_array << m[0].tr("\n", '')
   end
-  break
+  # break
 end
 question_blocks_array.each do |el|
   el.scan(regexp_question) do |m|
@@ -22,13 +22,17 @@ question_blocks_array.each do |el|
   end
   el.scan(regexp_answer) do |m|
     puts m.class.to_s
-      m.each do |q|
-        puts q.class.to_s
-        puts q
-        answers_array << q.strip
-      end
- #   answers_array << m[0].strip
+    # m.each do |q|
+    #   puts q.class.to_s
+    #   puts q[0]
+    #   answers_array << q.strip
+    # end
+    answers_array << m[0].strip
   end
-  puts answers_array.inspect
+
 end
-#puts question_blocks_array.inspect
+puts answers_array.size.to_s
+answers_array.each do |q|
+  puts q
+end
+# puts question_blocks_array.inspect
